02d0143b88172d559ca9a7f4f50f556c4127e408
ExN4Z <ichayuli80@gmail.com>
2026-08-12 10:13:57 +0700

    -

--- app/Models/JenisAset.php
+++ app/Models/JenisAset.php
@@ -8,10 +8,22 @@ class JenisAset extends Model
 {
     protected $table = 'jenis_aset';
 
-    protected $fillable = ['nama'];
+    protected $fillable = ['nama', 'kategori'];
 
     public function aset()
     {
         return $this->hasMany(Aset::class, 'jenis_id');
+    }
+
+    // scope buat filter dropdown Tambah Aset (pilih jenis aset utama) vs
+    // checklist kelengkapan di form peminjaman (pilih jenis kelengkapan).
+    public function scopeAsetUtama($query)
+    {
+        return $query->where('kategori', 'aset_utama');
+    }
+
+    public function scopeKelengkapan($query)
+    {
+        return $query->where('kategori', 'kelengkapan');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\Karyawan\AdminUserController;
 use App\Http\Controllers\Inventaris\SupplierController;
 use App\Http\Controllers\Inventaris\JenisAsetController;
-use App\Http\Controllers\Inventaris\KelengkapanMasterController;
 use App\Http\Controllers\Inventaris\AsetController;
 use App\Http\Controllers\Inventaris\AsetPemakaiController;
 use App\Http\Controllers\Inventaris\AsetPenangananController;
@@ -100,7 +99,6 @@ Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
     Route::post('/aset-penanganan/{asetPenanganan}/terima', [AsetPenangananController::class, 'terima']); // admin: terima & mulai tangani laporan
     Route::post('/aset-penanganan/{asetPenanganan}', [AsetPenangananController::class, 'update']);
     Route::post('/import-aset', [ImportController::class, 'import']); // pakai POST + _method=PUT biar konsisten sama pola aset/{aset}
-    Route::post('/import-aset-rapi', [ImportController::class, 'importRapi']); // format baru "Data Aset Rapi": 1 baris = 1 barang, kolom Kategori eksplisit
 });
 
 Route::middleware(['auth:sanctum', 'role:karyawan,manajer,hr,admin'])->group(function () {
